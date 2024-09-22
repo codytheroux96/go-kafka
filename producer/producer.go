11189577434a68ef154484ce394a2c5009bbdc74
codytheroux96 <cody.theroux3@gmail.com>
2024-09-22 16:36:08 -0400

    getting started

---
+++ producer/producer.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func mian() {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+	api.Post("/comments", createComment)
+	app.Listen(":3000")
+}
+
+func createComment(c *fiber.Ctx) error {
+	return nil
+}
